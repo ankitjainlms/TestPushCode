64321d56f6a2a35a233ec4350d3a35b6bcb14669
mudit jain <mudit.jain@lmsin.com>
2018-07-27 17:21:40 +0530

    code push

---
+++ TestPushCode/TestPushCode/Service.swift
@@ -0,0 +1,22 @@
+//
+//  Service.swift
+//  TestPushCode
+//
+//  Created by LMS on 27/07/18.
+//  Copyright © 2018 LMS. All rights reserved.
+//
+
+import Foundation
+public class Service{
+    
+    private init(){
+        
+    }
+    
+    public static func myFunction() -> String
+    {
+        return "My First framework is"
+    }
+    
+    
+}
---
+++ TestPushCode/TestPushCode/TestPushCode.h
@@ -0,0 +1,19 @@
+//
+//  TestPushCode.h
+//  TestPushCode
+//
+//  Created by LMS on 27/07/18.
+//  Copyright © 2018 LMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestPushCode.
+FOUNDATION_EXPORT double TestPushCodeVersionNumber;
+
+//! Project version string for TestPushCode.
+FOUNDATION_EXPORT const unsigned char TestPushCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPushCode/PublicHeader.h>
+
+
